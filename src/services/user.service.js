17a842c16ee62ec8d6f50e4f0811a15f6e86d156
vishal90909 <testmytest129@gmail.com>
2025-01-29 17:10:56 +0530

    added chnages

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,30 @@
-const { User } = require('../models');
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
+const { User, Otp } = require('../models');
+const logger = require('../config/logger');
+const emailService  = require('./email.service');
+
+const createUser = async (userBody) => {
+  if (await User.isEmailTaken(userBody.email)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+  };
+  if (await User.isPhoneNumberTaken(userBody.phoneNumber)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'PhoneNumber already taken');
+  };
+  const emailSentCode = await emailService.sendVerificationEmail(userBody.email);
+  console.log('emailSentCode', emailSentCode);
+  const otpObject = {
+    otp: emailSentCode,
+    email: userBody.email,
+    method: 'sign-up',
+    lastOtpSentTime: new Date(),
+    name: userBody.name,
+    phoneNumber: userBody.phoneNumber
+  };
+  await Otp.create(otpObject);
+  
+  return;
+};
 
 const fetchUser = async () => {
   return User.find({
@@ -34,9 +58,43 @@ const adminLogin = async (userBody) => {
   return User.findOne({email: userBody.email});
 };
 
+const queryUsers = async (filter, options) => {
+  const users = await User.paginate(filter, options);
+  return users;
+};
+
+const getUserById = async (id) => {
+  return User.findById(id);
+};
+
+const updateUserById = async (userId, updateBody) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  if (updateBody.email && (await User.isEmailTaken(updateBody.email, userId))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+  }
+  Object.assign(user, updateBody);
+  await user.save();
+  return user;
+};
+
+const deleteUserById = async (userId) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  await user.remove();
+  return user;
+};
 module.exports = {
+  deleteUserById,
+  updateUserById,
+  queryUsers,
   fetchUser,
   fetchUserById,
   adminLogin,
-  getUserByEmail
+  getUserByEmail,
+  createUser
 };
