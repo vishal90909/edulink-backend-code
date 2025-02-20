cb5879b84451ccc8746695f287064c1a6aef5052
vishal90909 <testmytest129@gmail.com>
2025-02-20 20:40:01 +0530

    added chnages

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -7,6 +7,6 @@ const userController = require('../../controllers/user.controller');
 const router = express.Router();
 
 router.get('/', auth('users'),  userController.fetchUser);
-router.get('/:id', auth('userById'), userController.fetchUserById);
+router.get('/:id', auth(), userController.fetchUserById);
 
 module.exports = router;
